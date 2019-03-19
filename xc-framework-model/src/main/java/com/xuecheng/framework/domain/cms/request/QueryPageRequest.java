fd7c6811296a36f2777a77e8c21c6b90a999d419
shindou_glo <1556319379@qq.com>
2019-03-19 19:46:31 +0800

    项目框架搭建完成，第一次提交

---
+++ xc-framework-model/src/main/java/com/xuecheng/framework/domain/cms/request/QueryPageRequest.java
@@ -0,0 +1,29 @@
+package com.xuecheng.framework.domain.cms.request;
+
+import com.xuecheng.framework.model.request.RequestData;
+
+/**
+ * @Auther : shindou
+ * @Date : 2019/3/19
+ * @Description : 功能描述
+ * @Version : 1.0
+ */
+public class QueryPageRequest extends RequestData {
+
+    //站点id
+    private String siteId;
+
+    //页面id
+    private String pageId;
+
+    //页面名称
+    private String pageName;
+
+    //别名
+    private String pageAliase;
+
+    //模板Id
+    private String templateId;
+
+
+}
