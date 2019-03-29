f51121b160ead6198d737a967303df038da84eb9
shindou_glo <1556319379@qq.com>
2019-03-30 00:27:26 +0800

    3.29日晚间 完善页面静态化

---
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/config/MongoConfig.java
@@ -0,0 +1,29 @@
+package com.xuecheng.manage_cms.config;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.gridfs.GridFSBucket;
+import com.mongodb.client.gridfs.GridFSBuckets;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Auther : shindou
+ * @Date : 2019/3/29
+ * @Description : 功能描述
+ * @Version : 1.0
+ */
+@Configuration
+public class MongoConfig {
+
+    @Value("${spring.data.mongodb.database}")
+    String db;
+
+    @Bean
+    public GridFSBucket getGridFsBucket(MongoClient mongoClient){
+        MongoDatabase mongoDatabase = mongoClient.getDatabase(db);
+        GridFSBucket gridFSBucket = GridFSBuckets.create(mongoDatabase);
+        return gridFSBucket;
+    }
+}
--- xc-service-manage-cms/src/test/java/com/xuecheng/manage_cms/dao/TestRestTemplate.java
+++ xc-service-manage-cms/src/test/java/com/xuecheng/manage_cms/dao/TestRestTemplate.java
@@ -1,13 +1,26 @@
 package com.xuecheng.manage_cms.dao;
 
+import com.mongodb.client.gridfs.GridFSBucket;
+import com.mongodb.client.gridfs.GridFSDownloadStream;
+import com.mongodb.client.gridfs.model.GridFSFile;
+import org.apache.commons.io.IOUtils;
+import org.bson.types.ObjectId;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.gridfs.GridFsResource;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -23,10 +36,45 @@ public class TestRestTemplate {
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    GridFsTemplate gridFsTemplate;
+
+    @Autowired
+    GridFSBucket gridFSBucket;
+
     @Test
     public void testRest(){
         String url = "http://localhost:31001/cms/config/getmodel/5a791725dd573c3574ee333f";
         ResponseEntity<Map> forEntity = restTemplate.getForEntity(url, Map.class);
         System.out.println(forEntity);
+    }
+
+    //测试使用GridFsTemplate 网mongoDB中存储文件
+    @Test
+    public void testGridFs() throws FileNotFoundException {
+        //要存储的文件
+        File file = new File("E:/XmyTemp/index_banner.html");
+        //获取输入流写入
+        FileInputStream fileInputStream = new FileInputStream(file);
+        //向GridFs中存入
+        ObjectId objectId = gridFsTemplate.store(fileInputStream, "轮播图测试by23");
+        //获取Id
+        System.out.println(objectId);
+    }
+
+    //测试使用GridFSBucket读取mongodb中存储的文件
+    @Test
+    public void testGridFSBucket() throws IOException {
+        //mongodb中存取的文件ID
+        String fileId = "5c9e3fd8ab2bdc1d68a1c792";
+        //根据ID查询文件
+        GridFSFile gridFSFile = gridFsTemplate.findOne(Query.query(Criteria.where("_id").is(fileId)));
+        //打开下载流对象
+        GridFSDownloadStream gridFSDownloadStream = gridFSBucket.openDownloadStream(gridFSFile.getObjectId());
+        //创建gridFsResource获取流对象
+        GridFsResource gridFsResource = new GridFsResource(gridFSFile,gridFSDownloadStream);
+        //获取流中的数据并打印
+        String s = IOUtils.toString(gridFsResource.getInputStream(),"UTF-8");
+        System.out.println(s);
     }
 }
